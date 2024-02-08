4a02d912efd1b8357fbd26379ffbae4fb93739f6
Hanny Tran <cheershanny@gmail.com>
2024-02-08 18:53:36 +0100

    Add 2 main components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
-import Image from "next/image";
+import Menu from "./Menu/MenuCard";
+import Order from "./Order/OrderCard";
 
 export default function Home() {
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="font-mono">Hello world!</h1>
-      <Image
-        src={"/han.png"}
-        alt={"picture of hanny"}
-        width={500}
-        height={500}
-      ></Image>
-
+    <main className="h-96 flex flex-row">
+      <Menu />
+      <Order />
     </main>
   );
 }
+ 
